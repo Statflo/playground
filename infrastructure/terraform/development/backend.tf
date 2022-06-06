3131345ebdffea8c2a7e8fbeea285b2c802a7e18
aumkarpraja <aumkarpraja@gmail.com>
2022-06-06 10:50:02 -0400

    Removed infra.

--- infrastructure/terraform/development/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "statflo-dev-terraform"
-    key     = "standalone-repo/playground.tfstate"
-    region  = "ca-central-1"
-    encrypt = true
-  }
-}
-
--- infrastructure/terraform/production/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "tf.ca.prod.stflo.io"
-    key     = "standalone-repo/playground.tfstate"
-    region  = "ca-central-1"
-    encrypt = true
-  }
-}
-
